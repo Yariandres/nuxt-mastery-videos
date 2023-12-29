f225cff10ec23bac91c4f5d871e500f6e4a588c3
Yari Herrera <yari@primeinsights.com>
2023-12-29 23:57:56 +0100

    Add useCourse composable and update dependencies

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,6 +1,26 @@
+<script setup>
+import { useCourse } from '~/composable/useCourse';
+
+const course = useCourse();
+const route = useRoute();
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  );
+});
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  );
+});
+</script>
+
 <template>
   <h2>Lesson</h2>
   <p>This is a lesson</p>
-  <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
-  <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+
+  <p>{{ chapter.title }}</p>
+  <p>{{ lesson.title }}</p>
 </template>
